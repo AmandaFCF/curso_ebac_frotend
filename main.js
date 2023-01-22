4acee40e2953808707a357a60ff33e14937d8805
AmandaFCF <116609493+AmandaFCF@users.noreply.github.com>
2023-01-22 19:06:48 -0300

    Atualizado código

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
 $(document).ready(function(){
-    $('button').on('click', function() {
-
-        const newItem = $('#listItem').val()
-        const addNewItem = $('<li style="display: none"></li>');
-        const completedItem = $("li").val();
-
-        $(`
-            <li>
-                ${newItem}
-            </li>
-        `).appendTo(addNewItem);
-
-
-        $(addNewItem).append("ul");
-        $(addNewItem).fadeIn();
-        $("#listItem").val("");
-        
-        $("li").click(function() {
-            $("li").css("text-decoration", "line-throught");
-        })
-        })
-    }) 
+    $('form').submit(function(event){
+        event.preventDefault();
+        let task = $("input[name='tarefa']").val();
+        $("ul").append("<li>"+task+"</li>");
+        $("input[name='tarefa']").val("");
+    });
+    $("ul").on("click", "li", function(){
+        $(this).css("text-decoration", "line-through");
+    });
+});
